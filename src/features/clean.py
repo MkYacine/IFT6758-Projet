1dd752e4475239680f02cf7bcaaa8297273cd007
mk_ya <mk_yassin@hotmail.fr>
2023-11-09 15:56:48 -0500

    added powerplay data to entries

--- src/features/clean.py
+++ src/features/clean.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 def get_game_seconds(period : int, period_time : object):
     minutes, seconds = map(int, period_time.split(':'))
-    return (period-1)*900 + minutes*60 + seconds
+    return (period-1)*1200 + minutes*60 + seconds
 
 def safe_get(dct, keys, default=np.NaN):
     """
@@ -33,26 +33,66 @@ def clean_row(row):
     :return: dictionary containing data about all plays in one game
     """
     plays = []
+    all_plays_list = row['liveData']['plays']['allPlays']
+    boxscore = row['gameData']
+    home_team = safe_get(boxscore, ['teams', 'home', 'name'])
+
+    # Last event related variables
     last_event_type = np.nan
     last_x = np.nan
     last_y = np.nan
     last_period = np.nan
     last_period_time = "00:00"
-    all_plays_list = row['liveData']['plays']['allPlays']
+
+    # Powerplay related variables
+    home_players = 5
+    away_players = 5
+    powerplay_duration = 0
+    penalties = []
     
-    shots_and_goals = [play for play in all_plays_list if (play['result']['event'] in ['Shot', 'Goal'])]
     for play in all_plays_list:
+        play_time = get_game_seconds(safe_get(play, ['about', 'period']), safe_get(play, ['about', 'periodTime']))
+
+        # Remove expired penalties from the list
+        for i in range(len(penalties) - 1, -1, -1):  # Iterate in reverse order, to avoid index problems when using pop()
+            if penalties[i]['end'] <= play_time:
+                if penalties[i]['team'] == home_team:
+                    home_players += 1
+                else:
+                    away_players += 1
+                penalties.pop(i)
+
+
+        # Update powerplay timer
+        if home_players != away_players:
+            powerplay_duration += play_time - get_game_seconds(last_period, last_period_time)
+        else:
+            powerplay = 0
+
+
+        # Check if event is penalty
+        if play['result']['event'] == 'Penalty':
+            penalty =  {'end': play_time+ 60*play['result']['penaltyMinutes'],
+                       'team': play['team']['name']}
+            if penalty['team']==home_team:
+                home_players -= 1
+            else:
+                away_players -= 1
+            penalties.append(penalty)
+
+        # Check if event is shot or goal
         if (play['result']['event'] in ['Shot', 'Goal']):
             x_coord = safe_get(play, ['coordinates', 'x'])
             y_coord = safe_get(play, ['coordinates', 'y'])
             period = safe_get(play, ['about', 'period'])
             period_time = safe_get(play, ['about', 'periodTime'])
             distance = ((x_coord-last_x)**2 + (y_coord-last_y)**2)**(1/2)
-            time_elapsed = get_game_seconds(period, period_time) - get_game_seconds(last_period, last_period_time)
+            time_elapsed = get_game_seconds(period, period_time) - get_game_seconds(last_period, last_period_time)        
             play_data = {
                 'period': period,
                 'period_type': safe_get(play, ['about', 'periodType']),
                 'period_time': period_time,
+                'game_seconds': play_time,
                 'gameID': safe_get(row, ['gamePk']),
                 'attacking_team_id': safe_get(play, ['team', 'id']),
                 'attacking_team_name': safe_get(play, ['team', 'name']),
@@ -72,6 +112,9 @@ def clean_row(row):
                 'last_event_y': last_y,
                 'time_since_last_event': time_elapsed,
                 'distance_from_last_event': distance,
+                'powerplay_duration': powerplay_duration,
+                'home_team_players': home_players,
+                'away_team_players': away_players
             }
             plays.append(play_data)
     
