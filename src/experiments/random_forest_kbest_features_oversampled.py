efe42ff563cab8af9102c2bf6d4c02287d0804b9
Yaniiiuss <yanis.kahil01@gmail.com>
2023-11-12 18:29:40 -0500

    Updates

--- src/experiments/random_forest_kbest_features_oversampled.py
+++ src/experiments/random_forest_kbest_features_oversampled.py
@@ -18,7 +18,7 @@ def main():
     y_pred_proba = model.predict_proba(X_val)[:, 1]
 
     exp = create_experiment('RandomForest_kbest_features_SMOTE')
-    log_metrics(exp, y_val, y_pred, y_pred_proba)
+    log_metrics(exp, y_val.values, y_pred, y_pred_proba)
     log_plots(exp, y_val.values, y_pred_proba)
     log_model(exp, model, 'RandomForest_Kbest_features_oversampled', ["RandomForest", "Model With Oversampled data and features from K-best", "Features: Q4 set"])
     exp.end()
